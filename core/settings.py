4a657c458efa559b98fe73ac39ca108dd9152c82
tammam <tammamit4plus@gmail.com>
2024-10-16 23:05:09 +0300

    edit settings

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wz5@ih^1!*e4e0o%mqgnc=mo^isp&gy^3abp4$+ldd28h=q&%t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 # Application definition
